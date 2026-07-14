b5432cf82f24ca9c76dd2ecb109cd7184ede9f6b
Sesmarias <svania044@gmail.com>
2026-07-14 13:16:55 -0100

    Update Semafaro_3 (1).py

--- Semafaro_3 (1).py
+++ Semafaro_3 (1).py
@@ -120,15 +120,6 @@ if st.session_state.mensagem:
 # Desenho simples do tabuleiro 3x4.
 colv1, col1, col2, col3, col4, colv2 = st.columns(6)
 
-st.markdown("""
-<style>
-div[data-testid="stButton"] > button {
-    max-width: 50px;
-    max-height: 70px;
-    font-size: 30px;
-}
-</style>
-""", unsafe_allow_html=True)
 
 with col1:
     st.button(
@@ -136,6 +127,7 @@ with col1:
         key="celula_0",
         on_click=jogar,
         args=(0,),
+        max-height=30px
         use_container_width=True,
     )
 
